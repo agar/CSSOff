561daa0c26a59fa1470a91f81942e99eb700b7a7
Matt Agar <matt.agar@gmail.com>
2011-10-27 22:48:45 +1100

    Image optimisation, tidy ups.

--- HTML/js/libs.js
+++ HTML/js/libs.js
@@ -69,3 +69,63 @@ else var s=p/(2*Math.PI)*Math.asin(c/a);if(t<1)return-.5*(a*Math.pow(2,10*(t-=1)
  * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
  */
 ;(function(d){var k=d.scrollTo=function(a,i,e){d(window).scrollTo(a,i,e)};k.defaults={axis:'xy',duration:parseFloat(d.fn.jquery)>=1.3?0:1};k.window=function(a){return d(window)._scrollable()};d.fn._scrollable=function(){return this.map(function(){var a=this,i=!a.nodeName||d.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!i)return a;var e=(a.contentWindow||a).document||a.ownerDocument||a;return d.browser.safari||e.compatMode=='BackCompat'?e.body:e.documentElement})};d.fn.scrollTo=function(n,j,b){if(typeof j=='object'){b=j;j=0}if(typeof b=='function')b={onAfter:b};if(n=='max')n=9e9;b=d.extend({},k.defaults,b);j=j||b.speed||b.duration;b.queue=b.queue&&b.axis.length>1;if(b.queue)j/=2;b.offset=p(b.offset);b.over=p(b.over);return this._scrollable().each(function(){var q=this,r=d(q),f=n,s,g={},u=r.is('html,body');switch(typeof f){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(f)){f=p(f);break}f=d(f,this);case'object':if(f.is||f.style)s=(f=d(f)).offset()}d.each(b.axis.split(''),function(a,i){var e=i=='x'?'Left':'Top',h=e.toLowerCase(),c='scroll'+e,l=q[c],m=k.max(q,i);if(s){g[c]=s[h]+(u?0:l-r.offset()[h]);if(b.margin){g[c]-=parseInt(f.css('margin'+e))||0;g[c]-=parseInt(f.css('border'+e+'Width'))||0}g[c]+=b.offset[h]||0;if(b.over[h])g[c]+=f[i=='x'?'width':'height']()*b.over[h]}else{var o=f[h];g[c]=o.slice&&o.slice(-1)=='%'?parseFloat(o)/100*m:o}if(/^\d+$/.test(g[c]))g[c]=g[c]<=0?0:Math.min(g[c],m);if(!a&&b.queue){if(l!=g[c])t(b.onAfterFirst);delete g[c]}});t(b.onAfter);function t(a){r.animate(g,j,b.easing,a&&function(){a.call(this,n,b)})}}).end()};k.max=function(a,i){var e=i=='x'?'Width':'Height',h='scroll'+e;if(!d(a).is('html,body'))return a[h]-d(a)[e.toLowerCase()]();var c='client'+e,l=a.ownerDocument.documentElement,m=a.ownerDocument.body;return Math.max(l[h],m[h])-Math.min(l[c],m[c])};function p(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
+
+/**
+ * jQuery Hotkeys Plugin
+ * Copyright 2010, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Based upon the plugin by Tzury Bar Yochay:
+ * http://github.com/tzuryby/hotkeys
+ *
+ * Original idea by:
+ * Binny V A, http://www.openjs.com/scripts/events/keyboard_shortcuts/
+*/
+(function(jQuery){jQuery.hotkeys={version:"0.8",specialKeys:{8:"backspace",9:"tab",13:"return",16:"shift",17:"ctrl",18:"alt",19:"pause",20:"capslock",27:"esc",32:"space",33:"pageup",34:"pagedown",35:"end",36:"home",37:"left",38:"up",39:"right",40:"down",45:"insert",46:"del",96:"0",97:"1",98:"2",99:"3",100:"4",101:"5",102:"6",103:"7",104:"8",105:"9",106:"*",107:"+",109:"-",110:".",111:"/",112:"f1",113:"f2",114:"f3",115:"f4",116:"f5",117:"f6",118:"f7",119:"f8",120:"f9",121:"f10",122:"f11",123:"f12",144:"numlock",145:"scroll",191:"/",224:"meta"},shiftNums:{"`":"~","1":"!","2":"@","3":"#","4":"$","5":"%","6":"^","7":"&","8":"*","9":"(","0":")","-":"_","=":"+",";":": ","'":"\"",",":"<",".":">","/":"?","\\":"|"}};function keyHandler(handleObj){if(typeof handleObj.data!=="string"){return;}var origHandler=handleObj.handler,keys=handleObj.data.toLowerCase().split(" ");handleObj.handler=function(event){if(this!==event.target&&(/textarea|select/i.test(event.target.nodeName)||event.target.type==="text")){return;}var special=event.type!=="keypress"&&jQuery.hotkeys.specialKeys[event.which],character=String.fromCharCode(event.which).toLowerCase(),key,modif="",possible={};if(event.altKey&&special!=="alt"){modif+="alt+";}if(event.ctrlKey&&special!=="ctrl"){modif+="ctrl+";}if(event.metaKey&&!event.ctrlKey&&special!=="meta"){modif+="meta+";}if(event.shiftKey&&special!=="shift"){modif+="shift+";}if(special){possible[modif+special]=true;}else{possible[modif+character]=true;possible[modif+jQuery.hotkeys.shiftNums[character]]=true;if(modif==="shift+"){possible[jQuery.hotkeys.shiftNums[character]]=true;}}for(var i=0,l=keys.length;i<l;i++){if(possible[keys[i]]){return origHandler.apply(this,arguments);}}};}jQuery.each(["keydown","keyup","keypress"],function(){jQuery.event.special[this]={add:keyHandler};});})(jQuery);
+
+/**
+ * jQuery.poorMansTextShadow - Almost a text shadow in the underprivileged browsers (looking at you IE)
+ *
+ * Requires the Modernizer plugin to detect the support for text-shadow.
+ *
+ * Copyright 2011 Matt Agar
+ * Released under the MIT, BSD, and GPL Licenses.
+ */
+(function( $ ) {
+	$.fn.poorMansTextShadow = function( options ) {
+		if (Modernizr.textshadow) {
+			return;
+		}
+		var settings = {
+			'vertical' : 2,
+			'horizontal' : 2,
+			'color' : '#333333'
+		};
+		return this.each(function() {
+			if ( options ) {
+				$.extend( settings, options );
+			}
+			var $this = $(this),
+				xPadding = parseInt($this.css('padding-left')),
+				yPadding = parseInt($this.css('padding-top'));
+
+			// Add an absolutely positioned span
+			var $span = $('<span />').text($this.text()).css({
+				'color': $this.css('color'),
+				'position': 'absolute',
+				'left': xPadding,
+				'top': yPadding,
+				'width': $this.width(),
+				'height': $this.height()
+			});
+
+			// Offset the text underneath and make it look shadowish
+			$this.append($span).css({
+				'color': settings.color,
+				'position': 'relative',
+				'paddingLeft': settings.horizontal + xPadding,
+				'paddingTop': settings.vertical  + yPadding
+			});
+		});
+	};
+})( jQuery );
--- HTML/js/script.js
+++ HTML/js/script.js
@@ -14,7 +14,7 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 // Resize sections to fit nicely...
 $(window).resize(function() {
 	var h = $(window).height();
-	$('header,section').css('min-height', (h - 14));
+	$('header,#obstacles,#prizes').css('min-height', (h - 14));
 	$('header h1 span').css('height', Math.min($('header h1 span').width() - 40, 425));
 }).resize();
 
@@ -25,12 +25,64 @@ $('nav a').click(function(e) {
 	$.scrollTo($($(this).attr('href')), 1000, {easing:'easeOutQuint'});
 });
 
+// Window scroll
+$(window).scroll(function(e) {
+	var top = $(document).scrollTop();
+	var wHeight = Math.max($('section').height(), $(window).height());
+	var current = Math.floor(top / wHeight + 0.4);
+	$('header,section,footer').removeClass('current').slice(current, current+1).addClass('current');
+	var i = $('.current').attr('id')
+	$('nav a').removeClass('selected');
+	if (i != undefined && i != 'top') {
+		$('nav a[href="#'+i+'"]').addClass('selected');
+	}
+});
+
+// Keyboard navigation
+$(document).bind('keydown',function(e) {
+	switch(e.keyCode) {
+		case 32: // Space
+		case 40: // Down
+			e.preventDefault();
+			target = $('.current').next('section,footer').attr('id');
+			if (target != undefined) {
+				$('nav a[href="#'+target+'"]').click();
+			}
+			break;
+		case 38: // Up
+			e.preventDefault();
+			target = $('.current').prev('section,footer').attr('id');
+			if (target != undefined) {
+				$('nav a[href="#'+target+'"]').click();
+			} else if($('.current').attr('id') == 'obstacles') {
+				$('nav a[href="#top"]').click();
+			}
+		case 37: // Left
+			if ($('.current').attr('id') == 'obstacles') {
+				e.preventDefault();
+				target = $('#obstacles li.selected').prev('li');
+				if (target) target.find('a').click();
+			}
+			break;
+		case 39: // Right
+			if ($('.current').attr('id') == 'obstacles') {
+				e.preventDefault();
+				target = $('#obstacles li.selected').next('li');
+				if (target) target.find('a').click();
+			}
+			break;
+	}
+});
+
+// TODO: Add left/right/swipe switching between obstacles.
+
 // Obstacle scrolling
 $('#obstacles ul a').click(function(e) {
 	e.preventDefault();
 	$('#obstacles .panel').stop().scrollTo($($(this).attr('href')), 500, {easing:'easeOutCubic'});
+	$('#obstacles li').removeClass('selected');
+	$(this).parent().addClass('selected');
 });
-// TODO: Add left/right/swipe switching between obstacles.
 
 // The Final Countdown - de ne neerrr nerrrrr!
 var countdownTimer = setInterval(function() {
@@ -38,12 +90,17 @@ var countdownTimer = setInterval(function() {
 	if (v < 0) {
 		$('.countdown').addClass('done');
 		clearInterval(countdownTimer);
-
-		// TODO: Something grand when the timer stops...
+		$('#participate h2').html('Time&lsquo;s up!!!');
+		// TODO: Something grand when the timer stops... slime splodge perhaps - "Be quick!"
 	} else {
 		$('.countdown').text(v);
 	}
 }, 1000);
+
+// Super basic text shadow for the underprivileged
+$('header h2').poorMansTextShadow({color: '#ffffff', 'vertical': 3, 'horizontal': 3});
+$('section h1').poorMansTextShadow({color: '#333333', 'vertical': 3, 'horizontal': 3});
+
 
 
 
